9963ecf6636dc72cae4e9e3a5d8b5b32b8441352
maximeri <t.s.eliot1996@gmail.com>
2022-02-20 17:28:14 +0800

    feat: add body-parser

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express =require('express')
+const {engine} = require('express-handlebars')
 const app = express()
 const PORT = 3000
+const bodyParser = require('body-parser')
+const routes = require('./routes')
 require('./config/mongoose')
 
-app.get('/', (req,res)=>{
-  res.send('hello')
-})
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', './views');
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(routes)
+
 
 app.listen(PORT, ()=>{
   console.log(`App is running on localhost:${PORT}`)
