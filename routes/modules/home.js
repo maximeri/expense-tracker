6031147380a20aac25f85c85e4b42efe92c6ac1e
maximeri <t.s.eliot1996@gmail.com>
2022-02-24 19:54:12 +0800

    feat: add sorting by category function

---
+++ routes/modules/home.js
@@ -0,0 +1,92 @@
+const express = require('express')
+const router = express.Router()
+const Record = require('../../models/record')
+const moment = require('moment')
+
+router.get('/', async (req, res) => {
+  await Record.find().lean()
+  .then(records=> {
+    let totalAmount = 0
+    records.forEach(record => {
+      const amount = record.amount
+      record.date = moment(record.date).format('YYYY-MM-DD')
+      totalAmount = totalAmount + amount
+    })
+    res.render('index', { records, totalAmount})
+  }) 
+  .catch(error=>console.log(error))
+})
+
+
+// sort
+router.get('/sortBy=household', async (req, res) => {
+  await Record.find({categoryId:1}).lean()
+    .then(records => {
+      let totalAmount = 0
+      records.forEach(record => {
+        const amount = record.amount
+        record.date = moment(record.date).format('YYYY-MM-DD')
+        totalAmount = totalAmount + amount
+      })
+      res.render('index', { records, totalAmount })
+    })
+    .catch(error => console.log(error))
+})
+
+router.get('/sortBy=transportation', async (req, res) => {
+  await Record.find({ categoryId: 2 }).lean()
+    .then(records => {
+      let totalAmount = 0
+      records.forEach(record => {
+        const amount = record.amount
+        record.date = moment(record.date).format('YYYY-MM-DD')
+        totalAmount = totalAmount + amount
+      })
+      res.render('index', { records, totalAmount })
+    })
+    .catch(error => console.log(error))
+})
+
+router.get('/sortBy=entertainment', async (req, res) => {
+  await Record.find({ categoryId: 3 }).lean()
+    .then(records => {
+      let totalAmount = 0
+      records.forEach(record => {
+        const amount = record.amount
+        record.date = moment(record.date).format('YYYY-MM-DD')
+        totalAmount = totalAmount + amount
+      })
+      res.render('index', { records, totalAmount })
+    })
+    .catch(error => console.log(error))
+})
+
+router.get('/sortBy=food', async (req, res) => {
+  await Record.find({ categoryId: 4 }).lean()
+    .then(records => {
+      let totalAmount = 0
+      records.forEach(record => {
+        const amount = record.amount
+        record.date = moment(record.date).format('YYYY-MM-DD')
+        totalAmount = totalAmount + amount
+      })
+      res.render('index', { records, totalAmount })
+    })
+    .catch(error => console.log(error))
+})
+
+router.get('/sortBy=other', async (req, res) => {
+  await Record.find({ categoryId: 5}).lean()
+    .then(records => {
+      let totalAmount = 0
+      records.forEach(record => {
+        const amount = record.amount
+        record.date = moment(record.date).format('YYYY-MM-DD')
+        totalAmount = totalAmount + amount
+      })
+      res.render('index', { records, totalAmount })
+    })
+    .catch(error => console.log(error))
+})
+
+module.exports = router
