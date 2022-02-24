77e5615ab1dd369accf8b269a213786747f41b83
maximeri <t.s.eliot1996@gmail.com>
2022-02-24 19:56:56 +0800

    feat: add ifEqual handlbars helper

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ const {engine} = require('express-handlebars')
 const app = express()
 const PORT = 3000
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 const routes = require('./routes')
 require('./config/mongoose')
-
+const Handlebars = require("handlebars")
 app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', './views');
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(routes)
 
+module.exports = Handlebars.registerHelper("ifEqual", function (v1, v2) {
+  if (v1 === v2)
+    return true
+});
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+app.use(routes)
 
 app.listen(PORT, ()=>{
   console.log(`App is running on localhost:${PORT}`)
