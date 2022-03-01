cd4cf99c2e1285f1e4081f71ea2cb22fc5ea4013
maximeri <t.s.eliot1996@gmail.com>
2022-03-01 19:25:19 +0800

    feat: add user logou feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,20 +1,47 @@
 const express = require('express')
 const router = express.Router()
-const Record = require('../../models/record')
 const User = require('../../models/user')
 const bcrypt = require('bcrypt')
+const passport = require('passport')
 
+//register
 router.get('/register',(req,res)=>{
   res.render('register')
 })
 
 router.post('/register', async (req, res) => {
-  
-  const {name,email} = req.body
-  await bcrypt.genSalt(10)
-    .then(salt => bcrypt.hash(req.body.password,salt))
-    .then(hash => User.create({ name, email, password: hash})) 
-    .then(() => res.redirect('/'))
+  const {name,email, password} = req.body
+  await User.findOne({email})
+  .then(user=>
+    {
+      if(!user) {
+      bcrypt.genSalt(10)
+      .then(salt => bcrypt.hash(req.body.password,salt))
+      .then(hash => User.create({ name, email, password: hash})) 
+      .then(() => res.redirect('/'))} else {
+        console.log('User already exists.')
+        res.redirect('register',{name,email,password})
+        .catch(err => console.log(err))
+      }
+  })
+})
+
+// login
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+router.post('/login',
+  passport.authenticate('local', { 
+    successRedirect:'/',
+    failureRedirect: '/users/login',
+  })
+);
+
+// logout
+router.get('logout',(req,res)=>{
+  req.logout()
+  res.redirect('/users/login')
 })
 
 module.exports = router
