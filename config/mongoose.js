5911d9cbc9973433d0cae258375ea687ac7eb6d7
maximeri <t.s.eliot1996@gmail.com>
2022-04-11 23:30:59 +0800

    fix: error

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose')
 MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/expense-list'
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).catch (error => handleError(error))
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 
 // connection events
-db.once('open',()=>{
-  console.log('mongodb connected!')
+db.on('error', () => {
+  console.log('mongodb error!')
 })
 
-db.on('error', err => {
-  console.log('mongodb error!')
-});
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 module.exports = db
